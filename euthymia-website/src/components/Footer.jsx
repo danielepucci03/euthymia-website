3761ee8c6dfcacf8b2ceebf987fb3d66493c6e5e
danielepucci03 <danielepucci03@gmail.com>
2026-07-21 23:20:31 +0200

    feat: Update Footer component layout and styles; add contact icons and improve responsiveness

--- euthymia-website/src/components/Footer.jsx
+++ euthymia-website/src/components/Footer.jsx
@@ -1,10 +1,56 @@
 import imageLogo from '../assets/EUTHYMIALOGOSALVIA.svg'
+import { BsInstagram, BsWhatsapp, BsTelephone, BsMailbox } from "react-icons/bs";
+
 
 function Footer() {
     return (
         <section>
 
-            <div className="container container-footer">
+            <div className="container">
+
+                <div className="container-footer">
+
+                    <div className="contact-list">
+
+                        <div className="contact-item">
+
+                            <a href="tel:+393291624932">
+                                <BsTelephone />
+                            </a>
+                        </div>
+
+                        <div className="contact-item">
+
+                            <a href="mailto:danielepucci03@gmail.com">
+                                <BsMailbox />
+                            </a>
+                        </div>
+
+                        <div className="contact-item">
+
+                            <a
+                                href="https://wa.me/393291624932"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <BsWhatsapp />
+                            </a>
+                        </div>
+
+                        <div className="contact-item">
+
+                            <a
+                                href="https://www.instagram.com/_.danielepucci._"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <BsInstagram />
+                            </a>
+                        </div>
+
+
+                    </div>
+                </div>
 
                 <div className="footer-bottom">
 
@@ -16,51 +62,6 @@ function Footer() {
                     <a href="">Cookie Policy</a>
 
                 </div>
-
-                <div className="contact-list">
-
-                    <div className="contact-item">
-                        <h3>WhatsApp</h3>
-
-                        <a
-                            href="https://wa.me/393291624932"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Scrivici su WhatsApp
-                        </a>
-                    </div>
-
-                    <div className="contact-item">
-                        <h3>Instagram</h3>
-
-                        <a
-                            href="https://www.instagram.com/_.danielepucci._"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            @_.danielepucci._
-                        </a>
-                    </div>
-
-                    <div className="contact-item">
-                        <h3>Telefono</h3>
-
-                        <a href="tel:+393291624932">
-                            +39 329 162 4932
-                        </a>
-                    </div>
-
-                    <div className="contact-item">
-                        <h3>Email</h3>
-
-                        <a href="mailto:danielepucci03@gmail.com">
-                            danielepucci03@gmail.com
-                        </a>
-                    </div>
-
-                </div>
-
 
             </div>
 
